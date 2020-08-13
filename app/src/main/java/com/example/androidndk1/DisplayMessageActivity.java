501ef51f2a91a45fb82fdbfc6a2e3ad9e3c04a08
Abraham Qian <qianyonglian@outlook.com>
2020-08-13 18:56:16 +0800

    tweak

--- app/src/main/java/com/example/androidndk1/DisplayMessageActivity.java
+++ app/src/main/java/com/example/androidndk1/DisplayMessageActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCenter.start(getApplication(), "0748a395-56d6-4ae0-bda2-0efdf14b7dca",
+                Analytics.class, Crashes.class);
+
         setContentView(R.layout.activity_display_message);
 
         // Get the Intent that started this activity and extract the string
