82a3be7442fafab38c5f75aa89a0d246ba30e310
Abraham Qian <qianyonglian@outlook.com>
2020-08-14 14:43:25 +0800

    tweak java time

--- app/src/main/java/com/example/androidndk1/MainActivity.java
+++ app/src/main/java/com/example/androidndk1/MainActivity.java
@@ -13,6 +13,7 @@ import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void sendMessage(View view){
+        SimpleDateFormat sdf=new SimpleDateFormat();
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss a");
         Date d=new Date();
-        Analytics.trackEvent("button is clicked at "+ d.getTime());
+        Analytics.trackEvent("button is clicked at "+sdf.format(d));
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
